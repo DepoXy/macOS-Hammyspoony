651e10c6d6473740f315c2fad5a4c2800558a185
Landon Bouma <landonb@users.noreply.github.com>
2025-09-30 23:14:49 -0500

    docs: update ref. to renamed resource

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -551,7 +551,7 @@ local ctrl_backtick = hs.hotkey.bind({ "ctrl" }, "`", function()
   -- - USYNC: The is the classic nvim-depoxy setup I use for notes
   --   (at least until I port those feature into my LazyVim setup):
   --     DEPOXY_NVIM_ALTERNATE="🦢"
-  --   ~/.depoxy/ambers/core/alias-vim.sh
+  --   ~/.depoxy/ambers/core/launchvim.sh
   hs.window.find("🦢"):raise():focus()
 end)
 -- HSTRY/2025-02-25: Was previously <Shift-Cmd-Tilde> aka <D-~>
